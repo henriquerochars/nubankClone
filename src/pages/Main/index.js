9c2a9a1d611e23493e4fb464416b924148b249a0
Henrique Rocha Serrano <henriquerochaserranoh@gmail.com>
2021-03-26 00:39:34 -0300

    (feat) - create Card component (Content, Card, CardHeader, CardContent, Title, Description, CardFooter and Annotation components)

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import Header from './../../components/Header';
 import Tabs from './../../components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197,611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Henrique Rocha hoje as
+              8:00h;
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
