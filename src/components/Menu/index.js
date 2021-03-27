250622a922d36c5ecf517a844a340b3a2a3a2ee5
Henrique Rocha Serrano <henriquerochaserranoh@gmail.com>
2021-03-27 18:58:33 -0300

    create Nav components and SignOut Button components

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,13 +1,51 @@
 import React from 'react'
 import QRCode from 'react-native-qrcode-svg'
 
-import { Container, Code } from './styles'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import {
+  Container,
+  Code,
+  Nav,
+  NavItem,
+  NavText,
+  SignOutButton,
+  SignOutButtonText,
+} from './styles'
 
 const Menu = () => (
   <Container>
     <Code>
-      <QRCode value="https://github.com/henriquerochars" size={80} backgroundColor="#8B10AE" color="#FFF" />
+      <QRCode
+        value="https://github.com/henriquerochars"
+        size={80}
+        backgroundColor="#8B10AE"
+        color="#FFF"
+      />
     </Code>
+
+    <Nav>
+      <NavItem>
+        <Icon name="help-outline" size={20} color="#FFF" />
+        <NavText>Me ajuda</NavText>
+      </NavItem>
+      <NavItem>
+        <Icon name="person-outline" size={20} color="#FFF" />
+        <NavText>Perfil</NavText>
+      </NavItem>
+      <NavItem>
+        <Icon name="credit-card" size={20} color="#FFF" />
+        <NavText>Configurar cartão</NavText>
+      </NavItem>
+      <NavItem>
+        <Icon name="smartphone" size={20} color="#FFF" />
+        <NavText>Configurações do app</NavText>
+      </NavItem>
+    </Nav>
+
+    <SignOutButton onPress={() => {}}>
+      <SignOutButtonText>Sair</SignOutButtonText>
+    </SignOutButton>
   </Container>
 )
 
